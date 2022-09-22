55053d616bbe12d9a81957fb074d3f1a59ab445f
Mike <mihailfd@gmail.com>
2022-09-22 16:02:10 +0200

    1

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 
 TOKEN = "5726668567:AAEnt_hRMirItfq2Jrn-srH8OGRz_oVjNOI"
 HEROKU_APP = "chat-bot-yuki"
-LOCAL = True 
+LOCAL = False 
 MONGODB_CONNECTION_STRING = 'mongodb+srv://user:user@cluster0.fnguq3m.mongodb.net/?retryWrites=true&w=majority'
 
