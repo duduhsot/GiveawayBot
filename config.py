1ade364020d3b9ab8f1eb39bd9bc6d6233734a88
duduhsot <115173530+duduhsot@users.noreply.github.com>
2022-10-07 10:26:14 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 
 TOKEN = "5648600381:AAG3MP_PJGGBeqsmcm_HT0EgWxGYS8SGCe0"
-HEROKU_APP = "dudushotgg"
+HEROKU_APP = "dudushotgg1"
 LOCAL = False 
 MONGODB_CONNECTION_STRING = 'mongoDB-connection-string'
 
